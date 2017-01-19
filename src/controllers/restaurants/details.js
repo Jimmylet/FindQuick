6b7ca809215172bc9193ddb2d42e34e3107f6bf3
Jimmy Letecheur <letecheur.j@gmail.com>
2017-01-19 11:42:26 +0100

    Compute distance in restaurant details

--- src/controllers/restaurants/details.js
+++ src/controllers/restaurants/details.js
@@ -1,13 +1,25 @@
 import getRestaurants from "../../models/restaurants";
 import { send, error } from "../../core/utils/api";
 import { ObjectID } from "mongodb";
+import distance from "jeyo-distans";
+
 
 export default function ( oRequest, oResponse ) {
 
-    let sRestaurantID = ( oRequest.params.id || "" ).trim();
+    let sRestaurantID = ( oRequest.params.id || "" ).trim(),
+        iLatitude = +oRequest.query.latitude,
+        iLongitude = +oRequest.query.longitude,
+        oCurrentPosition;
 
     if ( !sRestaurantID ) {
         error( oRequest, oResponse, "Invalid ID!", 400 );
+    }
+
+    if ( !isNaN( iLatitude ) && !isNaN( iLongitude ) ) {
+        oCurrentPosition = {
+            "latitude": iLatitude,
+            "longitude": iLongitude,
+        };
     }
 
     getRestaurants()
@@ -26,6 +38,10 @@ export default function ( oRequest, oResponse ) {
             oRestaurantData = {
                 "id": _id,
                 name, latitude, longitude, address, hours
+            }
+
+            if ( oCurrentPosition ){
+                oRestaurantData.distance = distance( oCurrentPosition, oRestaurantData ) * 1000;
             }
 
             send ( oRequest, oResponse, oRestaurantData );
