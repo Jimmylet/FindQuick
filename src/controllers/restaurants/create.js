a61de2773de8baf8a10f975c630cde38a958cee6
Jimmy Letecheur <letecheur.j@gmail.com>
2017-01-19 14:28:27 +0100

    implement terminal creation routes et properties

---
+++ src/controllers/restaurants/create.js
@@ -0,0 +1,51 @@
+import Promise from "bluebird";
+import { ObjectID } from "mongodb";
+
+import getRestaurants from "../../models/restaurants";
+import { send, error } from "../../core/utils/api";
+import checkPosition from "../../core/utils/position";
+
+export default function ( oRequest, oResponse ) {
+
+    const POST = oRequest.body;
+
+    let iLatitude = +POST.latitude,
+        iLongitude = +POST.longitude,
+        sAddress = ( POST.address || "" ).trim(),
+        sName = ( POST.name || "" ).trim(),
+        sSlug = ( POST.name ).replace(/\s+/g, '-').toLowerCase(),
+        aHours = ( POST.hours || "" ),
+        oPosition = checkPosition( iLatitude, iLongitude ),
+        oRestaurant;
+
+    if ( !oPosition ) {
+        return error( oRequest, oResponse, "Invalid position", 400 );
+    }
+
+
+    oRestaurant = {
+        "latitude": oPosition.latitude,
+        "longitude": oPosition.longitude,
+        "created_at": new Date(),
+        "updated_at": new Date()
+    };
+
+    sAddress && ( oRestaurant.address = sAddress );
+    sName && ( oRestaurant.name = sName );
+    sSlug && ( oRestaurant.name = sSlug );
+    aHours && ( oRestaurant.hours = aHours );
+
+    getRestaurants()
+        .insertOne( oRestaurant )
+        .then( () => {
+            send( oRequest, oResponse, {
+                "id": oRestaurant._id,
+                "name": oRestaurant.name || null,
+                "address": oRestaurant.address || null,
+                "hours": oRestaurant.hours,
+                "latitude": oRestaurant.latitude,
+                "longitude": oRestaurant.longitude,
+            }, 201 );
+        })
+        .catch( ( oError ) => error( oRequest, oResponse, oError ) );
+}
--- src/routes/restaurants.js
+++ src/routes/restaurants.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import list from "../controllers/restaurants/list";
 import details from "../controllers/restaurants/details";
-// import create from "../controllers/restaurants/create";
+import create from "../controllers/restaurants/create";
 // import update from "../controllers/restaurants/update";
 // import destroy from "../controllers/restaurants/destroy";
 
@@ -10,7 +10,7 @@ let oRouter = new Router();
 
 oRouter.get( "/restaurants", list );
 oRouter.get( "/restaurants/:id", details );
-// oRouter.post( "/restaurants", create );
+oRouter.post( "/restaurants", create );
 // oRouter.patch( "/restaurants", update );
 // oRouter.delete( "/restaurants", destroy );
 
