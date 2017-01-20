b99c452381912224d9cf7b71c56d34dabfaaf858
Jimmy Letecheur <letecheur.j@gmail.com>
2017-01-20 16:58:21 +0100

    Uncomment routes destroy et update

--- src/routes/restaurants.js
+++ src/routes/restaurants.js
@@ -3,15 +3,15 @@ import { Router } from "express";
 import list from "../controllers/restaurants/list";
 import details from "../controllers/restaurants/details";
 import create from "../controllers/restaurants/create";
-// import update from "../controllers/restaurants/update";
-// import destroy from "../controllers/restaurants/destroy";
+import update from "../controllers/restaurants/update";
+import destroy from "../controllers/restaurants/destroy";
 
 let oRouter = new Router();
 
 oRouter.get( "/restaurants", list );
 oRouter.get( "/restaurants/:id", details );
 oRouter.post( "/restaurants", create );
-// oRouter.patch( "/restaurants", update );
-// oRouter.delete( "/restaurants", destroy );
+oRouter.patch( "/restaurants/:id", update );
+oRouter.delete( "/restaurants/:id", destroy );
 
 export default oRouter;
