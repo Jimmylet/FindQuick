0293ab12aa94039bea29ee987a81466c3e35f993
Jimmy Letecheur <letecheur.j@gmail.com>
2017-01-20 16:57:08 +0100

    create fonction checkRestaurant in models

--- src/models/restaurants.js
+++ src/models/restaurants.js
@@ -1,5 +1,42 @@
 import { db } from "../core/mongodb";
+import Promise from "bluebird";
+import { ObjectID } from "mongodb";
+
+let fCheckRestaurant;
+
+
+
+
+fCheckRestaurant = function( sRestaurantID ) {
+    let oRestaurantID;
+
+    if ( !sRestaurantID ) {
+        return Promise.resolve( false );
+    }
+
+    try {
+        oRestaurantID = new ObjectID( sRestaurantID );
+    } catch ( oError ) {
+        return Promise.reject( new Error( "Invalid Bank ID!" ) );
+    }
+
+    return db.collection( "restaurants" )
+        .findOne( {
+            "_id": oRestaurantID,
+        } )
+        .then( ( oRestaurant ) => {
+            if ( oRestaurant ) {
+                return Promise.resolve( true );
+            }
+
+            return Promise.reject( new Error( "Unknown Restaurant!" ) );
+        } );
+};
 
 export default function() {
     return db.collection( "restaurants" );
 }
+
+export {
+    fCheckRestaurant as checkRestaurant,
+};
